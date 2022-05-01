d6f11a9f4c0e7f4ae4def377e93fc9283c49b2ce
Aaron Wright <aaron@sikosoft.com>
2022-05-01 21:10:18 +0200

    1.0.2: added roles definition

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const rights = require("./rights.json");
+const roles = require("./roles.json");
 const settings = require("./settings.json");
 const typeMap = require("./typeMap.json");
 
 module.exports = {
   rights,
+  roles,
   settings,
   typeMap,
 };
