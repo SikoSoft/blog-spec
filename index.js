a8ab7760d6a26c2866577e57d14b80fce9492cab
Aaron Wright <aaron@sikosoft.com>
2022-05-01 21:28:04 +0200

    1.0.3: added errorCodes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const errorCodes = require("./errorCodes.json");
 const rights = require("./rights.json");
 const roles = require("./roles.json");
 const settings = require("./settings.json");
 const typeMap = require("./typeMap.json");
 
 module.exports = {
+  errorCodes,
   rights,
   roles,
   settings,
