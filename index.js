f4f378f47d936bf67ed456a25a9fd20b842ba7e3
Aaron Wright <aaron@sikosoft.com>
2022-05-01 17:33:29 +0200

    1.0.1: adds typeMap, minor settings definition tweaks

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const rights = require("./rights.json");
 const settings = require("./settings.json");
+const typeMap = require("./typeMap.json");
 
 module.exports = {
   rights,
   settings,
+  typeMap,
 };
