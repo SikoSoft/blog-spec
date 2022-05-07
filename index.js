54523d10b2c0443faa293b0735ec921417ce3658
Aaron Wright <aaron@sikosoft.com>
2022-05-07 07:48:25 +0200

    2.2.0: expose version of the spec as a property of the spec

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { version } = require("./package.json");
 const errorCodes = require("./errorCodes.json");
 const rights = require("./rights.json");
 const roles = require("./roles.json");
@@ -5,6 +6,7 @@ const settings = require("./settings.json");
 const typeMap = require("./typeMap.json");
 
 module.exports = {
+  version,
   errorCodes,
   rights,
   roles,
