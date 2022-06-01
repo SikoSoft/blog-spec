f768dbad370f50397fa6a128c36b1051ccd06346
Aaron Wright <aaron@sikosoft.com>
2022-06-01 19:07:08 +0200

    3.0.0: all models now use dataType for columns

--- blog-spec.d.ts
+++ blog-spec.d.ts
@@ -2,6 +2,21 @@ export interface BlogContext {
   id: string;
 }
 
+export interface DatabaseTableColumn {
+  id: string;
+  type: string;
+  dataType?: string;
+  default?: number | string;
+  maxLength?: number;
+  primary?: boolean;
+  autoIncremenet?: boolean;
+}
+
+export interface DatabaseTable {
+  id: string;
+  attributes: [DatabaseTableColumn];
+}
+
 export declare const contexts: Array<BlogContext>;
 export declare const errorCodes: Record<string, number>;
 export declare const rights: Array<any>;
