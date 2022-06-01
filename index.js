f0ce150257889523151a0609aeeb97a10b8cb8d1
Aaron Wright <aaron@sikosoft.com>
2022-06-01 12:52:55 +0200

    2.8.1: expose models in export

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const glob = require("glob");
+const path = require("path");
 const { version, displayName } = require("./package.json");
 const contexts = require("./contexts.json");
 const errorCodes = require("./errorCodes.json");
@@ -5,6 +7,12 @@ const rights = require("./rights.json");
 const roles = require("./roles.json");
 const settings = require("./settings.json");
 const typeMap = require("./typeMap.json");
+
+const models = glob
+  .sync(path.join(__dirname, "models/**/*.json"))
+  .map(function (file) {
+    return require(path.resolve(file));
+  });
 
 const spec = {
   version,
@@ -15,6 +23,7 @@ const spec = {
   roles,
   settings,
   typeMap,
+  models,
 };
 
 module.exports = spec;
