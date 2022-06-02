7cf47a604cac8a4ee5035fcdbed2ad677031e482
Aaron Wright <aaron@sikosoft.com>
2022-06-02 08:49:44 +0200

    3.0.3: added hardcoded list of models to import/export & angry comment that this is the state of working in Node

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const glob = require("glob");
-const path = require("path");
 const { version, displayName } = require("./package.json");
 const contexts = require("./contexts.json");
 const errorCodes = require("./errorCodes.json");
@@ -8,11 +6,34 @@ const roles = require("./roles.json");
 const settings = require("./settings.json");
 const typeMap = require("./typeMap.json");
 
-const models = glob
-  .sync(path.join(__dirname, "models/**/*.json"))
-  .map(function (file) {
-    return require(path.resolve(file));
-  });
+// I absofuckinglylutely hate that I have to hardcode a god damn list of files because
+// there is no fucking consistent supported way to import files from a directory, without
+// dragging in a fucking library for something as trivial as this
+// Webpack no longer supports polyfilling node packages!
+const modelsList = [
+  "blocks_content",
+  "blocks_context",
+  "blocks",
+  "comments_scores",
+  "comments",
+  "entries_tags",
+  "entries",
+  "filters_rules",
+  "filters",
+  "image_sizes",
+  "roles_rights",
+  "roles",
+  "settings",
+  "tags_rights",
+  "tags",
+  "tokens_consumed",
+  "tokens_invalid_attempts",
+  "tokens",
+];
+
+const models = modelsList.map(function (modelName) {
+  return require(`./models/core/${modelName}.json`);
+});
 
 const spec = {
   version,
